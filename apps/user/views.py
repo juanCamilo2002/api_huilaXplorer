abaf10a8a786c937f7446efb1663cf87e4b6e43b
juanCamilo2002 <juan2002djfh@gmail.com>
2024-10-04 16:53:36 -0500

    fix: create user

--- apps/user/views.py
+++ apps/user/views.py
@@ -1,10 +1,11 @@
 from rest_framework.views import APIView
 from rest_framework import viewsets, status, permissions, filters
 from rest_framework.response import Response
+from rest_framework import generics
 from django.contrib.auth import get_user_model
 from .utils import send_verification_code, send_reset_pass_code
 from django.utils import timezone
-from .serializers import UserSerializer, VerifyCodeSerializer, SendCodeSerializer, ResetPasswordSerializer, ErrorResponseSerializer, SuccessResponseSerializer
+from .serializers import UserSerializer, VerifyCodeSerializer, SendCodeSerializer, ResetPasswordSerializer, ErrorResponseSerializer, SuccessResponseSerializer, UserCreateAdminSerializer
 from .custom_permissions import IsAdminOrIsSelf
 from drf_spectacular.utils import extend_schema
 
@@ -162,3 +163,18 @@ class UserViewSet(viewsets.ModelViewSet):
             permission_classes = [permissions.IsAdminUser]
 
         return [permission() for permission in permission_classes]
+    
+
+class CreateUserAdminAccountView(generics.CreateAPIView):
+    serializer_class = UserCreateAdminSerializer
+    permission_classes = [permissions.IsAdminUser]
+
+class ActiveAccountAdminView(APIView):
+    def get(self, request, user_id):
+        try:
+            user = User.objects.get(id=user_id)
+            user.is_active = True
+            user.save()
+            return Response({'message': 'Account activated successfully'}, status=status.HTTP_200_OK)
+        except User.DoesNotExist:
+            return Response({'error': 'User not found'}, status=status.HTTP_404_NOT_FOUND)
